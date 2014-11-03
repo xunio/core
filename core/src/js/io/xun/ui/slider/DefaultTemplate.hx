d61a31bb8277491ba49f66059229f18885f8b8b6
Hans Mackowiak <hanmac@gmx.de>
2014-11-03 14:44:37 +0100

    wrote the DefaultTemplate to implement the ISliderTemplate

--- core/src/js/io/xun/ui/slider/DefaultTemplate.hx
+++ core/src/js/io/xun/ui/slider/DefaultTemplate.hx
@@ -1,7 +1,43 @@
 package js.io.xun.ui.slider;
 
-class DefaultTemplate implements IStage
+import js.JQuery;
+import js.html.Element;
+
+class DefaultTemplate implements ISliderTemplate
 {
-	public function new( ) {
+    var _container : JQuery;
+
+    public function addStage(stage:IStage):Void {
+    }
+
+    public function switchStage(stagePosition:Int):Void {
+    }
+
+    public function getContainer():Element {
+        return _container.get(0);
+    }
+
+    public function getStagesContainer():Element {
+        return _container.find(".stagesContainer").get(0);
+    }
+
+    public function getStageContainer(stage:IStage):Element {
+        return stage.getContent();
+    }
+
+    public function getNextButton():Null<Element> {
+        return _container.find(".next").get(0);
+    }
+
+    public function getPrevButton():Null<Element> {
+        return _container.find(".prev").get(0);
+    }
+
+    public function getSlideButtonContainer():Null<Element> {
+        return _container.find(".slideButtons").get(0);
+    }
+
+    public function new(mainElement : Element) {
+        _container = new JQuery(mainElement);
 	}
 }
