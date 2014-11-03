7ac1df494a88693e79cd13c38ae91d5b129ea177
Hans Mackowiak <hanmac@gmx.de>
2014-11-03 16:35:41 +0100

    DefaultStage now has container for the stage and its button, the template does add it later to the html

--- core/src/js/io/xun/ui/slider/DefaultStage.hx
+++ core/src/js/io/xun/ui/slider/DefaultStage.hx
@@ -1,29 +1,38 @@
 package js.io.xun.ui.slider;
 
 import js.html.Element;
+
 class DefaultStage implements IStage
 {
     var _slider : ISlider;
+    var stageContainer : JQuery;
+    var buttonContainer : JQuery;
 
     public function setSlider(slider:ISlider):Void {
         _slider = slider;
     }
 
-    public function getContent():Element {
-        return js.Browser.document.body;
+    public function getContainer():Element {
+        return stageContainer.get(0);
+    }
+    public function getButton(): Null<Element> {
+        return buttonContainer.get(0);
     }
 
     public function show():Void {
-        new JQuery(getContent()).show();
+        new JQuery(getContainer()).show();
     }
 
     public function hide():Void {
-        new JQuery(getContent()).hide();
+        new JQuery(getContainer()).hide();
     }
 
     public function initialize():Void {
     }
 
-    public function new( ) {
+    public function new( content : String ) {
+        stageContainer = new JQuery('<li>' + content + '</li>');
+
+        buttonContainer = new JQuery('<li>' + content + '</li>');
 	}
 }
